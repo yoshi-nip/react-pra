4464e6e92d50ce67a44c7a9509ccdf5f47d25a4c
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-14 21:31:51 +0900

    スタイルの当て方について確認

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const App = () => {
   const onClickButton = () =>alert()
+  const cssSample = {
+    color : 'red',
+    fontSize: '50px'
+  };
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>エクスポートとインポートの確認</p>
+      <h1 style={cssSample}>こんにちは</h1>
+      <p>スタイルの当て方について確認</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   )
