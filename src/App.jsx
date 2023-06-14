cb2bcf12edee8a4abf422ea1f336bba564725c2d
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-14 21:11:25 +0900

    イベントの書き方について確認

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () =>alert()
   return (
     <>
       <h1>こんにちは</h1>
       <p>エクスポートとインポートの確認</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
-  );
+  )
 };
 
 export default App;
