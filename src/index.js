541de0d05c3bd36238126c23002ee6536d4c9aa7
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-14 20:42:40 +0900

    エクスポートとインポートの確認

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
-// import React, { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "./styles.css";
-
-// import App from "./App";
-
-// const root = createRoot(document.getElementById("root"));
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
 import React from "react";
 import ReactDom from "react-dom";
 
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは</h1>
-      <p>テストp</p>
-    </>
-  );
-};
+import App from "./App";
+
 ReactDom.render(<App />, document.getElementById("root"));
