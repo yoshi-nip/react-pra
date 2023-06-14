38374bfcc296809d58cdba0e3e2e8451ef6150e8
yoshi <mdmdmdmdmd57@gmail.com>
2023-06-14 22:42:01 +0900

    onOffボタンと3の倍数を関連づけた時のバグの例を実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,26 @@ import React, { useState } from "react";
 import Fruit from "./compoent/Fruit";
 
 const App = () => {
-  const onClickButton = () =>{
-    setNum(num+1)
-  }
-  const onOffButton = () =>{
-    if(num % 3  === 0){
-      setOnOff(!onOff)
-    }
-  }
   const [num,setNum] = useState(0)
   const [onOff,setOnOff] = useState(true)
   const cssSample = {
     color : 'red',
     fontSize: '50px'
   };
+  const onClickButton = () =>{
+    setNum(num+1)
+  }
+  const onOffButton = () =>{
+      setOnOff(!onOff)
+  }
+  if(num > 0){
+    if(num % 3 === 0){
+      onOff || setOnOff(true)
+    }else{
+      onOff && setOnOff(false)
+    }
+  }
+
   return (
     <>
       <h1 style={cssSample}>こんにちは</h1>
